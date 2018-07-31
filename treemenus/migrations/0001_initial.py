a61d954c409a5afc99d5712d54d9c54117d999ad
pacahon <pacabest@gmail.com>
2018-07-31 11:00:03 +0300

    suppress deprecation warning in migration

--- treemenus/migrations/0001_initial.py
+++ treemenus/migrations/0001_initial.py
@@ -31,12 +31,12 @@ class Migration(migrations.Migration):
                 ('level', models.IntegerField(default=0, verbose_name='level', editable=False)),
                 ('rank', models.IntegerField(default=0, verbose_name='rank', editable=False)),
                 ('menu', models.ForeignKey(related_name='contained_items', blank=True, editable=False, to='treemenus.Menu', null=True, verbose_name='menu')),
-                ('parent', models.ForeignKey(verbose_name='parent', blank=True, to='treemenus.MenuItem', null=True)),
+                ('parent', models.ForeignKey(verbose_name='parent', on_delete=django.db.models.deletion.PROTECT, blank=True, to='treemenus.MenuItem', null=True)),
             ],
         ),
         migrations.AddField(
             model_name='menu',
             name='root_item',
-            field=models.ForeignKey(related_name='is_root_item_of', blank=True, editable=False, to='treemenus.MenuItem', null=True, verbose_name='root item'),
+            field=models.ForeignKey(related_name='is_root_item_of', on_delete=django.db.models.deletion.PROTECT, blank=True, editable=False, to='treemenus.MenuItem', null=True, verbose_name='root item'),
         ),
     ]
